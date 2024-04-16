654a90e7cef713d7e2d7b4d1b0e9fb55f4c9d569
n1co02 <nicolas.ostermann8702@gmail.com>
2024-04-16 17:20:02 +0200

    Implement binary search and linear search algorithms***
    ***Add optimized algorithm for determining the exact spot where crystal balls break

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,13 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
-
+    let low = 0;
+    let high = haystack.length;
+    while (low < high) {
+        //weil ich nur haystack.length verwende nicht haystack.length-1
+        const mid = Math.floor(low + (high - low) / 2);
+        const value = haystack[mid];
+        if (value === needle) return true;
+        if (value > needle) high = mid;
+        if (value < needle) low = mid + 1;
+    }
+    return false;
 }
--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,3 +1,18 @@
+//given two crystal calls that will break if dropped from high enough distance
+//determine the exact spot in which it will break in the most optimized way
 export default function two_crystal_balls(breaks: boolean[]): number {
-
+    const jmpAmount = Math.floor(Math.sqrt(breaks.length));
+    let i = jmpAmount;
+    for (; i < breaks.length; i += jmpAmount) {
+        if (breaks[i]) {
+            break;
+        }
+    }
+    i -= jmpAmount;
+    for (let j = 0; j < jmpAmount && i + j < breaks.length; ++j, ++i) {
+        if (breaks[i]) {
+            return i;
+        }
+    }
+    return -1;
 }
