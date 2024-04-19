54d0e9dbe22e1f44fe6b4aa6e4d52d382ba80bbe
n1co02 <nicolas.ostermann8702@gmail.com>
2024-04-19 18:38:43 +0200

    Refactor bubble sort algorithm to use array destructuring

--- src/day1/BubbleSort.ts
+++ src/day1/BubbleSort.ts
@@ -2,9 +2,7 @@ export default function bubble_sort(arr: number[]): void {
     for (let i = 0; i < arr.length; i++) {
         for (let j = i + 1; j < arr.length; j++) {
             if (arr[i] > arr[j]) {
-                let temp = arr[i];
-                arr[i] = arr[j];
-                arr[j] = temp;
+                [arr[i], arr[j]] = [arr[j], arr[i]];
             }
         }
     }
