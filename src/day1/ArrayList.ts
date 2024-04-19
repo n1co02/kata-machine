e87d3013c2acd16accbba019f422e24f069c50a5
n1co02 <nicolas.ostermann8702@gmail.com>
2024-04-19 14:24:24 +0200

    implement ArrayList class to include length property and add prepend, insertAt, and removeAt methods

--- src/day1/ArrayList.ts
+++ src/day1/ArrayList.ts
@@ -1,27 +1,45 @@
 export default class ArrayList<T> {
     public length: number;
+    public data: T[];
 
-    
-
-    constructor() {
+    constructor(length: number) {
+        this.length = length;
+        this.data = [];
     }
 
     prepend(item: T): void {
+        this.data.unshift(item);
+        this.length = this.data.length;
+    }
 
-}
     insertAt(item: T, idx: number): void {
+        if (idx < 0 || idx > this.data.length)
+            throw new Error("Index out of bounds");
 
-}
+        this.data.splice(idx, 0, item);
+        this.length = this.data.length;
+    }
+
     append(item: T): void {
-
-}
+        this.data.push(item);
+        this.length = this.data.length;
+    }
     remove(item: T): T | undefined {
+        const filteredItem = this.data.indexOf(item);
+        if (filteredItem === -1) return undefined;
+        this.length--;
+        return this.data.splice(filteredItem, 1)[0];
+    }
 
-}
     get(idx: number): T | undefined {
+        return this.data[idx];
+    }
 
-}
     removeAt(idx: number): T | undefined {
-
-}
+        if (idx < 0 || idx >= this.data.length) {
+            return undefined;
+        }
+        this.length--;
+        return this.data.splice(idx, 1)[0];
+    }
 }
