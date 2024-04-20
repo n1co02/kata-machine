8c6e49a2b3cdbd14562e364a96de0ff9cf48befb
n1co02 <nicolas.ostermann8702@gmail.com>
2024-04-20 20:35:50 +0200

    Refactor solve function and add walk function

--- src/day1/MazeSolver.ts
+++ src/day1/MazeSolver.ts
@@ -1,3 +1,22 @@
-export default function solve(maze: string[], wall: string, start: Point, end: Point): Point[] {
+export default function solve(
+    maze: string[],
+    wall: string,
+    start: Point,
+    end: Point,
+): Point[] {}
 
-}
+const walk = (
+    maze: string[],
+    wall: string,
+    curr: Point,
+    end: Point,
+): boolean => {
+    if (
+        curr.x < 0 ||
+        curr.x >= maze[0].length ||
+        curr.y < 0 ||
+        curr.y >= maze.length
+    )
+        return false;
+    return true;
+};
