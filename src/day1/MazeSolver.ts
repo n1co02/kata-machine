59e65a9e1bec9933ae6a56dc0018678d36283b28
n1co02 <nicolas.ostermann8702@gmail.com>
2024-04-21 14:26:26 +0200

    Add path tracking to MazeSolver

--- src/day1/MazeSolver.ts
+++ src/day1/MazeSolver.ts
@@ -1,9 +1,24 @@
+const dir = [
+    [-1, 0],
+    [1, 0],
+    [0, -1],
+    [0, 1],
+];
+
 export default function solve(
     maze: string[],
     wall: string,
     start: Point,
     end: Point,
-): Point[] {}
+): Point[] {
+    const seen: boolean[][] = [];
+    const path: Point[] = [];
+    for (let i = 0; i < maze.length; i++) {
+        seen.push(new Array(maze[0].length).fill(false));
+    }
+    walk(maze, wall, start, end, seen, path);
+    return path;
+}
 
 const walk = (
     maze: string[],
@@ -11,6 +26,7 @@ const walk = (
     curr: Point,
     end: Point,
     seen: boolean[][],
+    path: Point[],
 ): boolean => {
     //check if of the map
     if (
@@ -23,9 +39,21 @@ const walk = (
 
     if (maze[curr.y][curr.x] === wall) return false; //if wall
 
-    if (curr.x === end.x && curr.y === end.y) return true; //if end
+    if (curr.x === end.x && curr.y === end.y) {
+        path.push(end);
+        return true;
+    } //if end
 
     if (seen[curr.y][curr.x]) return false; //if already visited
+    seen[curr.y][curr.x] = true;
+    path.push(curr);
 
-    return true;
+    for (let i = 0; i < dir.length; i++) {
+        const [x, y] = dir[i];
+        if (walk(maze, wall, { x: curr.x + x, y: curr.y + y }, end, seen, path))
+            return true;
+    }
+
+    path.pop();
+    return false;
 };
