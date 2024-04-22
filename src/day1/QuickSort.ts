8d100bb32f0f58a1a4535af1e4d88e45917dd521
n1co02 <nicolas.ostermann8702@gmail.com>
2024-04-22 17:41:52 +0200

    Refactor QuickSort implementation

--- src/day1/QuickSort.ts
+++ src/day1/QuickSort.ts
@@ -1,3 +1,28 @@
 export default function quick_sort(arr: number[]): void {
+    qs(arr, 0, arr.length - 1);
+}
 
+function qs(arr: number[], low: number, high: number): void {
+    if (low < high) {
+        const pi = partition(arr, low, high);
+        qs(arr, low, pi - 1);
+        qs(arr, pi + 1, high);
+    }
+}
+
+function partition(arr: number[], low: number, high: number): number {
+    const pivot = arr[high];
+    let idx = low - 1;
+
+    for (let i = low; i < high; i++) {
+        if (arr[i] <= pivot) {
+            idx++;
+            [arr[i], arr[idx]] = [arr[idx], arr[i]];
+        }
+    }
+    idx++;
+    arr[high] = arr[idx];
+    arr[idx] = pivot;
+
+    return idx;
 }
