9ebff54a007a9945a255ef6ff03eec8c04907941
n1co02 <nicolas.ostermann8702@gmail.com>
2024-04-24 17:06:21 +0200

    btbfs

--- src/day1/BTBFS.ts
+++ src/day1/BTBFS.ts
@@ -1,3 +1,10 @@
 export default function bfs(head: BinaryNode<number>, needle: number): boolean {
-
+    const queue: (BinaryNode<number> | null)[] = [head];
+    while (queue.length) {
+        let current = queue.shift() as BinaryNode<number>;
+        if (current?.value === needle) return true;
+        if (current.left) queue.push(current.left);
+        if (current.right) queue.push(current.right);
+    }
+    return false;
 }
