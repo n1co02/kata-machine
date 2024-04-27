1bb591fab237c2690667f4b5950d1db910115c89
n1co02 <nicolas.ostermann8702@gmail.com>
2024-04-27 11:59:12 +0200

    dfs on bst

--- src/day1/DFSOnBST.ts
+++ src/day1/DFSOnBST.ts
@@ -1,3 +1,9 @@
 export default function dfs(head: BinaryNode<number>, needle: number): boolean {
+    if (!head) return false;
 
+    if (head.value === needle) return true;
+
+    if (head.value < needle && head.right) return dfs(head.right, needle);
+    if (!head.left) return false;
+    return dfs(head.left, needle);
 }
