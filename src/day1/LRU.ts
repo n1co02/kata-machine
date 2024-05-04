2089f373184c3c1531a681a9ca0302aa6e93f3e7
n1co02 <nicolas.ostermann8702@gmail.com>
2024-05-04 13:52:12 +0200

    lru

--- src/day1/LRU.ts
+++ src/day1/LRU.ts
@@ -1,15 +1,85 @@
+type Node<T> = {
+    value: T;
+    next?: Node<T>;
+    prev?: Node<T>;
+};
+
 export default class LRU<K, V> {
     private length: number;
+    private head?: Node<V>;
+    private tail?: Node<V>;
 
-    
+    private lookup: Map<K, Node<V>>;
+    private reverseLookup: Map<Node<V>, K>;
 
-    constructor() {
+    constructor(private capacity: number) {
+        this.length = 0;
+        this.head = this.tail = undefined;
+        this.lookup = new Map<K, Node<V>>();
+        this.reverseLookup = new Map<Node<V>, K>();
     }
 
     update(key: K, value: V): void {
+        let node = this.lookup.get(key);
+        if (!node) {
+            node = createNode(value);
+            this.length++;
+            this.prepend(node);
+            this.trimCache();
+            this.lookup.set(key, node);
+            this.reverseLookup.set(node, key);
+        } else {
+            this.detach;
+            this.prepend;
+            node.value = value;
+        }
+    }
+    trimCache(): void {
+        if (this.length <= this.capacity) return;
 
-}
+        const tail = this.tail as Node<V>;
+        this.detach(this.tail as Node<V>);
+
+        const key = this.reverseLookup.get(tail) as K;
+        this.lookup.delete(key);
+        this.reverseLookup.delete(tail);
+        this.length--;
+    }
+
+    private detach(node: Node<V>) {
+        if (node.prev) node.prev.next = node.next;
+        if (node.next) node.next.prev = node.prev;
+
+        if (this.length === 1) this.tail = this.head = undefined;
+
+        if (this.head === node) this.head = this.head.next;
+
+        if (this.tail === node) this.tail = this.tail.prev;
+        node.next = node.prev = undefined;
+    }
+
+    private prepend(node: Node<V>) {
+        if (!this.head) {
+            this.head = this.tail = node;
+            return;
+        }
+        node.next = this.head;
+
+        this.head.prev = node;
+
+        this.head = node;
+    }
+
     get(key: K): V | undefined {
+        const node = this.lookup.get(key);
+        if (!node) return undefined;
+        this.detach(node);
+        this.prepend(node);
 
+        return node.value;
+    }
 }
+
+function createNode<V>(value: V): Node<V> {
+    return { value };
 }
